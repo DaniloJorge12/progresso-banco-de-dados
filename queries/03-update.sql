95b03a4eb82c5ec1271cece0a172cf5bdf3d82de
Danilo Jorge <danilojmini2@gmail.com>
2025-09-22 09:00:10 -0300

    Comandos iniciais de update

---
+++ queries/03-update.sql
@@ -0,0 +1,9 @@
+UPDATE usuarios SET username = 'ProGamer2025'
+WHERE username = 'NoobMaster';
+
+SELECT * FROM usuarios;
+
+UPDATE usuarios SET pontos = pontos + 1000;
+
+UPDATE usuarios SET email = 'master@gmail.com'
+WHERE email = 'master@email.com'
